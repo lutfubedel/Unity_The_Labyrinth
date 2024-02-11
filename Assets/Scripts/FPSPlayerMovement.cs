30af033e90a78ea5d206d79fd319d49c5a723bf1
lutfubedel <lutfubedel13@gmail.com>
2024-02-11 19:39:28 +0300

    Added some bug fixed

--- Assets/Scripts/FPSPlayerMovement.cs
+++ Assets/Scripts/FPSPlayerMovement.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class FPSPlayerMovement : MonoBehaviour
 {
 
-    CharacterController controller;
 
-    public float speed = 12f;
-    public float jumpHeight;
+    [Header("Move Variables")]
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private float jumpHeight;
+
+    [Header("Jump Variables")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
+
+    [Header("Bools")]
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool canJump;
+
+    [Header("Mouse Variables")]
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private Transform playerBody;
+
     private float gravity = -9.18f;
     private float groundDistance = 0.4f;
 
-    public Transform groundCheck;
-    public LayerMask groundMask;
-
-    public bool isGrounded;
-    public bool canJump;
-
     Vector3 velocity;
+    CharacterController controller;
+
+    float xRotation = 0f;
 
     private void Start()
     {
@@ -26,6 +36,14 @@ public class FPSPlayerMovement : MonoBehaviour
     }
 
     private void Update()
+    {
+        PlayerMove();
+        MouseLook();
+
+    }
+
+
+    private void PlayerMove()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -48,6 +66,18 @@ public class FPSPlayerMovement : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
 
+
+    private void MouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.Rotate(Vector3.up * mouseX);
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
 }
